cd830faafab99f25d019e674ca1dbe383788d756
jpbarddal4kst <jpbarddal@4kst.com>
2022-05-19 16:49:37 -0300

    uid gid url fixes

--- levanta_jupyter.sh
+++ levanta_jupyter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 CONTAINER_ID=$(docker ps | grep pyspark | head -n1 | cut -d " " -f1)
 TOKEN=$(docker logs  ${CONTAINER_ID} 2>&1 | grep "token=" | head -n1 | cut -d "=" -f2)
-firefox --new-tab http://localhost:50070 --new-tab http://localhost:8989 --new-tab http://localhost:8088 --new-tab http://localhost:50080?token=${TOKEN}
+firefox --url http://localhost:50070 http://localhost:8989 http://localhost:8088 http://localhost:50080?token=${TOKEN}
