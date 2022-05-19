61b8667f1812f689f2dda34c59d38e57c7eb98ba
jpbarddal4kst <jpbarddal@4kst.com>
2022-05-19 17:28:31 -0300

    limpa pasta compartilhada do hadoop

--- levanta_docker.sh
+++ levanta_docker.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 docker-compose down
+rm -rf ./data/datanode_1
+rm -rf ./data/datanode_2
+rm -rf ./data/namenode
+rm -rf ./data/nodemanager
+rm -rf ./data/nodemanager
+rm -rf ./data/resourcemanager
 docker-compose up &
